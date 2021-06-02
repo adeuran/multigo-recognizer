3969e0aac43d39f16a3efb005c35f7213c2f1131
HyeonbeomHeo <contact@snowytiger.me>
2021-06-02 20:37:21 +0900

    여러 이미지를 인식 후 처리

--- main.cpp
+++ main.cpp
@@ -1,25 +1,67 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <filesystem>
 
+using namespace std::filesystem;
 using namespace cv;
 using namespace std;
 
+// 상수 정의
+const string INPUT_DIR = "input";
+const string OUTPUT_DIR = "output";
+const string TEXT_DIR = "/text";
+const string IMAGE_DIR = "/image";
+
 Mat calcGrayHist(const Mat& img);
 void setPotition(char(*map)[19], Point point, bool is_white);
+void start(string filepath);
+
+
 
 int main()
 {
 
+	//폴더 준비
+	// 
+	// input 폴더가 없으면 오류
+	if (!exists(INPUT_DIR)) {
+		cout << "\"input\" folder not found!" << endl;
+		return -1;
+	}
+	// output 관련 폴더가 남아있으면 날려버림
+	if (exists(OUTPUT_DIR)) {
+		remove_all(OUTPUT_DIR);
+	}
+	// 날린 폴더를 다시 만듦
+	path dir(OUTPUT_DIR);
+	create_directory(dir);
+	dir = path(OUTPUT_DIR + TEXT_DIR);
+	create_directory(dir);
+	dir = path(OUTPUT_DIR + IMAGE_DIR);
+	create_directory(dir);
+
+	directory_iterator input_files(INPUT_DIR);
+
+	// input 폴더의 파일들 불러서 처리 시작
+	for (const directory_entry & a_file : input_files) {
+		cout << a_file << endl;
+		start(a_file.path().string());
+	}
+
+	return 0;
+}
+
+void start(string filepath) {
 	// 영상 불러오기
-	Mat src = imread("./input/test06.jpg", IMREAD_GRAYSCALE);
+	Mat src = imread(filepath, IMREAD_GRAYSCALE);
 
 	if (src.empty()) {
 		cerr << "Image load failed!" << endl;
-		return -1;
+		return;
 	}
-	
-	char map[19][19] = {0, };	// 돌들을 저장할 2차원 배열 생성(0으로 저장)
-	fill(&map[0][0], &map[18][18]+1, '.');	// .으로 초기화
+
+	char map[19][19] = { 0, };	// 돌들을 저장할 2차원 배열 생성(0으로 저장)
+	fill(&map[0][0], &map[18][18] + 1, '.');	// .으로 초기화
 
 	// 이진화된 영상 생성
 	Mat bin;
@@ -32,11 +74,11 @@ int main()
 	cvtColor(src, dst, COLOR_GRAY2BGR);
 	for (Vec3f c : circles) {
 		Point center(cvRound(c[0]), cvRound(c[1]));
-		Rect centerpoint (Rect(center.x-5,center.y-5,10, 10));
+		Rect centerpoint(Rect(center.x - 5, center.y - 5, 10, 10));
 		Mat hist = calcGrayHist(bin(centerpoint));
 		// 검은색이 더 많으면(검은돌이면)
-		if (hist.at<float>(0) > hist.at<float>(255)){
-			circle(dst, center, 3,Scalar(0, 0, 255), -1);	// 원을 그림
+		if (hist.at<float>(0) > hist.at<float>(255)) {
+			circle(dst, center, 3, Scalar(0, 0, 255), -1);	// 원을 그림
 			setPotition(map, center, false);	// 좌표에 표시
 		}
 		// 흰돌이 더 많으면(흰돌이면)
@@ -56,13 +98,7 @@ int main()
 		cout << endl;
 	}
 
-	imshow("src", src);
-	imshow("bin", bin);
-	imshow("dst", dst);
-
 	waitKey();
-
-	return 0;
 }
 
 Mat calcGrayHist(const Mat& img) {
@@ -77,6 +113,7 @@ Mat calcGrayHist(const Mat& img) {
 	return hist;
 }
 
+// 돌의 위치를 map에 좌표화
 void setPotition(char(*map)[19], Point point, bool is_white) {
 	int least[] = { 52, 76, 100, 124, 148, 172, 196, 220, 244, 268, 292, 316, 340, 364, 388, 412, 436, 460, 500 };
 	int pointarr[] = { point.x, point.y };	// 바둑알 center의 위치
@@ -99,3 +136,4 @@ void setPotition(char(*map)[19], Point point, bool is_white) {
 	map[xy[0]][xy[1]] = color;
 	return;
 }
+
